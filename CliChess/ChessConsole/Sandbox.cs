92b6f83c5efe534962a3c23859a9e9103d17de24
evanm4183 <evanm4183@gmail.com>
2023-12-29 17:21:30 -0600

    creating a sandbox class that will run in the console

---
+++ CliChess/ChessConsole/Sandbox.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessConsole
+{
+    public static class Sandbox
+    {
+        public static void Run()
+        {
+            Console.WriteLine("This is the sandbox.");
+        }
+    }
+}
